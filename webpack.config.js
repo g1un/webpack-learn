1d6710d66f0436e1aa42000b31f7a3ade3c354e4
Ilya Smal <ilyasmal@MBP-Ilya.Dlink>
2017-08-14 13:26:49 +0300

    Testing webpack-spritesmith

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+var SpritesmithPlugin = require('webpack-spritesmith');
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+});
+
+module.exports = {
+    entry: {
+        app: './src/js/app.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: '/dist'
+    },
+    module: {
+
+        rules: [
+            {
+                test: /\.js$/,
+                use:[
+                    {
+                        loader: 'babel-loader',
+                        options: {
+
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            // publicPath: 'img/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+
+                            name: '[name].[ext]'
+                        }
+                    }
+                ],
+                exclude: path.resolve(__dirname, 'src/index.html')
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        // new HtmlWebpackPlugin({
+        //     filename: 'users.html',
+        //     template: 'src/users.html',
+        //     chunks: []
+        // }),
+        new CleanWebpackPlugin(['dist']),
+        new SpritesmithPlugin({
+            src: {
+                cwd: path.resolve(__dirname, 'src/ico'),
+                glob: '*.+(png|jpg)'
+            },
+            target: {
+                image: path.resolve(__dirname, 'src/img/sprite/sprite.png'),
+                css: path.resolve(__dirname, 'src/css/sprite.scss')
+            },
+            apiOptions: {
+                cssImageRef: "../img/sprite/sprite.png"
+            }
+        })
+    ]
+};
